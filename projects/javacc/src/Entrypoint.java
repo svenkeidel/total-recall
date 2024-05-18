c6f05addbc4e7c10f18f8a7cd97c508005ebc862
anasattia <anas.attia123456789@gmail.com>
2024-05-18 16:54:39 +0200

    add: entry Point recurseDirectories

--- projects/javacc/src/Entrypoint.java
+++ projects/javacc/src/Entrypoint.java
@@ -1,5 +1,22 @@
-
-
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.parser.Main
 public class Entrypoint {
+
+
+    public static void recurseDirectories(File path) throws IOException {
+        for(File inputFile: path.listFiles()) {
+            if(inputFile.isFile()) {
+                String input = InputParser.parseString(Files.readAllBytes(inputFile.toPath()));
+                Entrypoint.entrypoint(input);
+            } else {
+                recurseDirectories(inputFile);
+            }
+        }
+    }
+    public static void main(String args[]) throws IOException {
+        recurseDirectories(new File(args[0]));
+    }
    
 }
