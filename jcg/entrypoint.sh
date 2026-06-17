bce89239a20e8a881a81c9f623c553e64231870b
Sven Keidel <svenkeidel@pm.me>
2026-06-17 08:37:55 +0200

    Fix Dynamic agent

--- jcg/entrypoint.sh
+++ jcg/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 TIMEOUT=$1
 MX=$2
@@ -7,4 +7,4 @@ shift 2
 timeout --foreground $TIMEOUT \
   sbt -java-home /opt/jdk-25.0.3+9 -J-Xmx${MX} \
     "; project jcg_evaluation; $*" \
-  | tee /log.txt
+  |& tee /log.txt
