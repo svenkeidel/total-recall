8425831818d33cfabb8c28a6e8a9577f1ee7e948
Sven Keidel <svenkeidel@pm.me>
2026-07-08 18:00:55 +0200

    Update fatjar corpus, improve entrypoints of proguard

--- projects/proguard-4.9/src/Entrypoint.java
+++ projects/proguard-4.9/src/Entrypoint.java
@@ -8,11 +8,45 @@ import proguard.*;
 
 class Entrypoint {
 
-    Path result;
+    static Path result;
+
+    private static final String optimizations =
+            "class/marking/final," +
+            "class/merging/vertical," +
+            "class/merging/horizontal," +
+            "field/removal/writeonly," +
+            "field/marking/private," +
+            "field/propagation/value," +
+            "method/marking/private," +
+            "method/marking/static," +
+            "method/marking/final," +
+            "method/removal/parameter," +
+            "method/propagation/parameter," +
+            "method/propagation/returnvalue," +
+            "method/inlining/short," +
+            "method/inlining/unique," +
+            "method/inlining/tailrecursion," +
+            "code/merging," +
+            "code/simplification/variable," +
+            "code/simplification/arithmetic," +
+            "code/simplification/cast," +
+            "code/simplification/field," +
+            "code/simplification/branch," +
+            "code/simplification/string," +
+            "code/simplification/advanced," +
+            "code/removal/advanced," +
+            "code/removal/simple," +
+            "code/removal/variable," +
+            "code/removal/exception," +
+            "code/allocation/variable";
+
 
     public static void entrypoint(Path inputJar) {
         Path jdk = Paths.get("/resources/rt.jar");
         Path outputJar = result.resolve("output.jar");
+        Path dump = result.resolve("dump");
+        Path configFile = result.resolve("configuration.pro");
+        Path mapping = result.resolve("mapping.txt");
 
         try {
             Configuration configuration = new Configuration();
@@ -20,16 +54,39 @@ class Entrypoint {
                     "-injars", inputJar.toString(),
                     "-outjars", outputJar.toString(),
                     "-libraryjars", jdk.toString() + "(!**.jar;!module-info.class)",
+                    "-verbose",
+                    "-target", "1.4",
+                    "-dump", dump.toString(),
+                    "-printseeds",
+                    "-printconfiguration", configFile.toString(),
+                    "-printmapping", mapping.toString(),
+                    "-optimizations", optimizations,
                     "-optimizationpasses", "2",
                     "-overloadaggressively",
                     "-allowaccessmodification",
                     "-mergeinterfacesaggressively",
-                    "-useuniqueclassmembernames",
-                    "-repackageclasses", "''",
-                    "-keep", "public", "class", "Entrypoint",
-                    "-keepattributes", "SourceFile,LineNumberTable,Signature,InnerClasses,EnclosingMethod,Deprecated,Annotation",
+
+                    "-keep", "class Entrypoint",
+
+                    "-keep", "class com.google.gson.** { *; }",
+                    "-keep", "class org.axiondb.** { *; }",
+                    "-keep", "class org.apache.batik.** { *; }",
+                    "-keep", "class org.hsqldb.** { *; }",
+                    "-keep", "class com.jasml.** { *; }",
+                    "-keep", "class com.javacc.** { *; }",
+                    "-keep", "class proguard.** { *; }",
+                    "-keep", "class edu.umd.cs.findbugs.** { *; }",
+                    "-keep", "class com.jakewharton.gradle.dependencies.** { *; }",
+                    "-keep", "class org.apache.xerces.** { *; }",
+
+                    "-keep", "class kotlin.jvm.functions.** { *; }",
+                    "-keep", "class kotlin.Metadata { *; }",
+                    "-keepattributes", "*Annotation*,Signature,InnerClasses,EnclosingMethod",
+                    "-keepattributes", "kotlin.Metadata,kotlin.annotations.**",
+
                     "-dontskipnonpubliclibraryclasses",
-                    "-dontskipnonpubliclibraryclassmembers"
+                    "-dontskipnonpubliclibraryclassmembers",
+                    "-dontwarn"
             },
                     System.getProperties());
             try {
@@ -45,6 +102,7 @@ class Entrypoint {
             deleteDirectoryContents(result.toFile(), true);
         }
     }
+
     public static void deleteDirectoryContents(File directoryToBeDeleted, Boolean root) {
         File[] allContents = directoryToBeDeleted.listFiles();
         if (allContents != null) {
@@ -56,6 +114,7 @@ class Entrypoint {
             directoryToBeDeleted.delete();
         }
     }
+
     public static void main(String[] args) throws Exception {
         Entrypoint.result = Files.createTempDirectory("result");
         try(Stream<Path> files = Files.walk(Paths.get(args[0]))) {
--- projects/proguard-7.9.1/src/Entrypoint.java
+++ projects/proguard-7.9.1/src/Entrypoint.java
@@ -3,16 +3,61 @@ import java.nio.file.*;
 import java.io.IOException;
 import java.util.stream.Stream;
 import java.util.ArrayList;
+import java.util.Set;
 
 import proguard.*;
 
 public class Entrypoint {
 
     private static Path result;
+    private static final String optimizations =
+            "library/gson,class/marking/final," +
+            "class/unboxing/enum," +
+            "class/merging/vertical," +
+            "class/merging/horizontal," +
+            "class/merging/wrapper," +
+            "field/removal/writeonly," +
+            "field/marking/private," +
+            "field/generalization/class," +
+            "field/specialization/type," +
+            "field/propagation/value," +
+            "method/marking/private," +
+            "method/marking/static," +
+            "method/marking/final," +
+            "method/marking/synchronized," +
+            "method/removal/parameter," +
+            "method/generalization/class," +
+            "method/specialization/parametertype," +
+            "method/specialization/returntype," +
+            "method/propagation/parameter," +
+            "method/propagation/returnvalue," +
+            "method/inlining/short,method/inlining/unique," +
+            "method/inlining/tailrecursion," +
+            "code/merging," +
+            "code/simplification/variable," +
+            "code/simplification/arithmetic," +
+            "code/simplification/cast," +
+            "code/simplification/field," +
+            "code/simplification/branch," +
+            "code/simplification/object," +
+            "code/simplification/string," +
+            "code/simplification/math," +
+            "code/simplification/advanced," +
+            "code/removal/advanced," +
+            "code/removal/simple," +
+            "code/removal/variable," +
+            "code/removal/exception," +
+            "code/allocation/variable";
+
     public static void entrypoint(Path inputJar) {
         Path jdk = Paths.get("/resources/jmods/");
 
         Path outputJar = result.resolve("output.jar");
+        Path dump = result.resolve("dump");
+        Path configFile = result.resolve("configuration.pro");
+        Path mapping = result.resolve("mapping.txt");
+
+        Set<String> backport = Set.of("axion-1.0-M2.jar", "batik-1.7.jar", "findbugs-1.3.9.jar", "hsqldb-2.0.0.jar", "jasml-0.10.jar", "javacc-5.0.jar", "proguard-4.9.jar", "xerces-2.10.0.jar");
 
         try {
             Configuration configuration = new Configuration();
@@ -20,16 +65,71 @@ public class Entrypoint {
                     "-injars", inputJar.toString(),
                     "-outjars", outputJar.toString(),
                     "-libraryjars", jdk.toString() + "(!**.jar;!module-info.class)",
-                    "-optimizationpasses", "3",
+                    "-verbose",
+                    backport.contains(inputJar.getFileName().toString()) ? "-target" : "", backport.contains(inputJar.getFileName().toString()) ? "1.6" : "",
+                    "-dump", dump.toString(),
+                    "-printseeds",
+                    "-printconfiguration", configFile.toString(),
+                    "-printmapping", mapping.toString(),
+                    "-optimizations", optimizations,
+                    "-optimizationpasses", "2",
+                    "-optimizeaggressively",
                     "-overloadaggressively",
                     "-allowaccessmodification",
                     "-mergeinterfacesaggressively",
-                    "-useuniqueclassmembernames",
-                    "-repackageclasses", "''",
-                    "-keep", "public", "class", "Entrypoint",
-                    "-keepattributes", "SourceFile,LineNumberTable,Signature,InnerClasses,EnclosingMethod,Deprecated,Annotation",
+
+                    "-keep", "class Entrypoint",
+
+                    "-keep", "class com.google.gson.** { *; }",
+                    "-keep", "class org.axiondb.** { *; }",
+                    "-keep", "class org.apache.batik.** { *; }",
+                    "-keep", "class org.hsqldb.** { *; }",
+                    "-keep", "class com.jasml.** { *; }",
+                    "-keep", "class com.javacc.** { *; }",
+                    "-keep", "class proguard.** { *; }",
+                    "-keep", "class edu.umd.cs.findbugs.** { *; }",
+                    "-keep", "class com.jakewharton.gradle.dependencies.** { *; }",
+                    "-keep", "class org.apache.xerces.** { *; }",
+
+                    "-keep", "class kotlin.jvm.functions.** { *; }",
+                    "-keep", "class kotlin.Metadata { *; }",
+                    "-keepattributes", "*Annotation*,Signature,InnerClasses,EnclosingMethod",
+                    "-keepattributes", "kotlin.Metadata,kotlin.annotations.**",
+
+                    "-assumenoexternalsideeffects class java.lang.StringBuilder {" +
+                    "    public java.lang.StringBuilder();" +
+                    "    public java.lang.StringBuilder(int);" +
+                    "    public java.lang.StringBuilder(java.lang.String);" +
+                    "    public java.lang.StringBuilder append(java.lang.Object);" +
+                    "    public java.lang.StringBuilder append(java.lang.String);" +
+                    "    public java.lang.StringBuilder append(java.lang.StringBuffer);" +
+                    "    public java.lang.StringBuilder append(char[]);" +
+                    "    public java.lang.StringBuilder append(char[], int, int);" +
+                    "    public java.lang.StringBuilder append(boolean);" +
+                    "    public java.lang.StringBuilder append(char);" +
+                    "    public java.lang.StringBuilder append(int);" +
+                    "    public java.lang.StringBuilder append(long);" +
+                    "    public java.lang.StringBuilder append(float);" +
+                    "    public java.lang.StringBuilder append(double);" +
+                    "    public java.lang.String toString();" +
+                    "}",
+                    "-assumenoexternalreturnvalues class java.lang.StringBuilder {" +
+                    "    public java.lang.StringBuilder append(java.lang.Object);" +
+                    "    public java.lang.StringBuilder append(java.lang.String);" +
+                    "    public java.lang.StringBuilder append(java.lang.StringBuffer);" +
+                    "    public java.lang.StringBuilder append(char[]);" +
+                    "    public java.lang.StringBuilder append(char[], int, int);" +
+                    "    public java.lang.StringBuilder append(boolean);" +
+                    "    public java.lang.StringBuilder append(char);" +
+                    "    public java.lang.StringBuilder append(int);" +
+                    "    public java.lang.StringBuilder append(long);" +
+                    "    public java.lang.StringBuilder append(float);" +
+                    "    public java.lang.StringBuilder append(double);" +
+                    "}",
+                    "-keepkotlinmetadata",
                     "-dontskipnonpubliclibraryclasses",
-                    "-dontskipnonpubliclibraryclassmembers"
+                    "-dontskipnonpubliclibraryclassmembers",
+                    "-dontwarn"
                 },
                 System.getProperties()
             );
