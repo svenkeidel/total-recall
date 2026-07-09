0c16ec1578ddbb3086a4e758954124508ce060ba
Sven Keidel <svenkeidel@pm.me>
2026-07-09 06:27:48 +0200

    Update findbugs entrypoint

--- projects/findbugs-1.3.9/prepare_dictionary.sh
+++ projects/findbugs-1.3.9/prepare_dictionary.sh
@@ -1,3 +1,3 @@
 DICT_DIR=$1
 
-cp -r $BASE_DIR/fuzzing-corpora/jvm-jars/dict.txt $DICT_DIR/dict.txt
+cp -r $BASE_DIR/fuzzing-corpora/fatjars/dict.txt $DICT_DIR/dict.txt
--- projects/findbugs-1.3.9/src/Entrypoint.java
+++ projects/findbugs-1.3.9/src/Entrypoint.java
@@ -16,28 +16,28 @@ public class Entrypoint {
                     "-experimental",
                     "-auxclasspath",
                     //"repo/annotations-1.3.9.jar:repo/asm-3.1.jar:repo/asm-analysis-3.1.jar:repo/asm-commons-3.1.jar:repo/asm-tree-3.1.jar:repo/asm-util-3.1.jar:repo/asm-xml-3.1.jar:repo/bcel-1.3.9.jar:repo/commons-lang-2.4.jar:repo/dom4j-1.6.1.jar:repo/findbugs.jar:repo/icu4j-2.6.1.jar:repo/jFormatString-1.3.9.jar:repo/jaxen-1.1.1.jar:repo/jdom-1.0.jar:repo/jsr305-1.3.9.jar:repo/xalan-2.6.0.jar:repo/xercesImpl-2.6.2.jar:repo/xml-apis-1.0.b2.jar:repo/xmlParserAPIs-2.6.2.jar:repo/xom-1.0.jar:"+
-                    "resources/findbugs/jdk6/charsets.jar:" +
-                    "resources/findbugs/jdk6/compilefontconfig.jar:" +
-                    "resources/findbugs/jdk6/dnsns.jar:" +
-                    "resources/findbugs/jdk6/localedata.jar:" +
-                    "resources/findbugs/jdk6/pulse-java.jar:" +
-                    "resources/findbugs/jdk6/sunjce_provider.jar:" +
-                    "resources/findbugs/jdk6/sunpkcs11.jar:" +
-                    "resources/findbugs/jdk6/indicim.jar:" +
-                    "resources/findbugs/jdk6/thaiim.jar:" +
-                    "resources/findbugs/jdk6/javazic.jar:" +
-                    "resources/findbugs/jdk6/jce.jar:" +
-                    "resources/findbugs/jdk6/jsse.jar:" +
-                    "resources/findbugs/jdk6/management-agent.jar:" +
-                    "resources/findbugs/jdk6/resources.jar:" +
-                    "resources/findbugs/jdk6/rhino.jar:" +
-                    "resources/findbugs/jdk6/rt.jar:" +
-                    "resources/findbugs/jdk6/US_export_policy.jar:" +
-                    "resources/findbugs/jdk6/local_policy.jar:" +
-                    "resources/findbugs/jdk6/dt.jar:" +
-                    "resources/findbugs/jdk6/jconsole.jar:" +
-                    "resources/findbugs/jdk6/sa-jdi.jar:" +
-                    "resources/findbugs/jdk6/tools.jar",
+                    "/resources/jdk6/charsets.jar:" +
+                    "/resources/jdk6/compilefontconfig.jar:" +
+                    "/resources/jdk6/dnsns.jar:" +
+                    "/resources/jdk6/localedata.jar:" +
+                    "/resources/jdk6/pulse-java.jar:" +
+                    "/resources/jdk6/sunjce_provider.jar:" +
+                    "/resources/jdk6/sunpkcs11.jar:" +
+                    "/resources/jdk6/indicim.jar:" +
+                    "/resources/jdk6/thaiim.jar:" +
+                    "/resources/jdk6/javazic.jar:" +
+                    "/resources/jdk6/jce.jar:" +
+                    "/resources/jdk6/jsse.jar:" +
+                    "/resources/jdk6/management-agent.jar:" +
+                    "/resources/jdk6/resources.jar:" +
+                    "/resources/jdk6/rhino.jar:" +
+                    "/resources/jdk6/rt.jar:" +
+                    "/resources/jdk6/US_export_policy.jar:" +
+                    "/resources/jdk6/local_policy.jar:" +
+                    "/resources/jdk6/dt.jar:" +
+                    "/resources/jdk6/jconsole.jar:" +
+                    "/resources/jdk6/sa-jdi.jar:" +
+                    "/resources/jdk6/tools.jar",
                     jar.toString(),
             });
         } catch (Throwable exc) {
@@ -61,7 +61,7 @@ public class Entrypoint {
     public static void patchBCELClasses() throws Throwable {
         String[] classes = new String[] {"java.io.Serializable", "java.util.Map", "java.util.Collection", "java.util.Comparator"};
         for(String clazz : classes) {
-            ClassParser parser = new ClassParser("/resources/findbugs/jdk6/rt.jar", clazz.replace('.', '/')+".class");
+            ClassParser parser = new ClassParser("/resources/jdk6/rt.jar", clazz.replace('.', '/')+".class");
             Repository.addClass(parser.parse());
         }
     }
