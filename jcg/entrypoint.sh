b9d9d1e7f9636052326a141c69a4e21e6cff25b5
Sven Keidel <svenkeidel@pm.me>
2026-07-11 08:43:23 +0200

    Doop Adapter: disable reflection support

--- jcg/entrypoint.sh
+++ jcg/entrypoint.sh
@@ -5,6 +5,10 @@ MX=$2
 shift 2
 
 timeout --foreground $TIMEOUT \
-  sbt -java-home /opt/jdk-25.0.3+9 -J-Xmx${MX} -Dcom.fasterxml.jackson.core.StreamReadConstraints.maxDepth=10000 \
+  sbt -java-home /opt/jdk-25.0.3+9 \
+      -J-Xmx${MX} \
+      -J-Xss1000m \
+      -Dplay.json.parser.maxNestingDepth=10000 \
+      -Dcom.fasterxml.jackson.core.StreamReadConstraints.maxDepth=10000 \
     "; project jcg_evaluation; $*" \
   |& tee /log.txt
